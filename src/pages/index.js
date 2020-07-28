790df41c9b279f0886c0aef059961a55033ab22e
zprial <zprial@gmail.com>
2020-07-28 15:46:29 +0800

    fix page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,5 @@ export default () => (
   <div style={{ color: `purple` }}>
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
 )
